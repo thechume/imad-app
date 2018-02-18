5e15f9de390491f8ae2537100ec17604b06e5e53
thechume <36409967+thechume@users.noreply.github.com>
2018-02-18 23:15:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,23 +3,27 @@ console.log('Loaded!');
 //move THUG cover
 var img = document.getElementById('THUG');
 var marginLeft=0;
+var flag=0;
 function moveRight () {
-        img.onclick = function () {
-        var interval = setInterval(moveLeft, 20);
-        }
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 function moveLeft () {
-    img.onclick = function () {
-        var interval = setInterval(moveRight, 20);
-    }
     marginLeft = marginLeft - 1;
     img.style.marginLeft = marginLeft - 'px';
 }
 
 img.onclick = function () {
-    var interval = setInterval(moveRight, 20);
+    if( flag == 1 )
+    {
+        var interval = setInterval(moveRight, 20);
+        flag=0;
+    }
+    else
+    {
+        var interval = setInterval(moveLeft, 20);
+        flag=1;
+    }
     //img.style.marginLeft = '100px';
 }
