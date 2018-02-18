7d96fc1e554ab602327cc30edfedd334aa94f057
thechume <36409967+thechume@users.noreply.github.com>
2018-02-18 23:32:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,31 +6,18 @@ var marginLeft=0;
 var flag=0;
 var interval;
 function moveRight () {
-    if( marginLeft == 150 ) {
-        flag=1;
-    }
-    else {
-        marginLeft = marginLeft + 1;
-        img.style.marginLeft = marginLeft + 'px';
-    }
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
 }
 
 function moveLeft () {
-    if( marginLeft == 10 ) {
-        flag=1;
-    }
-    else {
         marginLeft = marginLeft - 1;
         img.style.marginLeft = marginLeft - 'px';
-    }
 }
 
 img.onclick = function () {
-    if ( flag == 1)
-    {
+    interval = setInterval(moveRight, 20);
+    img.onclick = function () {
         interval = setInterval(moveLeft, 20);
-    }
-    else {
-        interval = setInterval(moveRight, 20);
     }
 }
