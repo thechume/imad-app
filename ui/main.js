6d82bf01da63eb0483f082797847165a1ee31963
thechume <36409967+thechume@users.noreply.github.com>
2018-02-18 22:49:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,6 @@ console.log('Loaded!');
 //move THUG cover
 var img = document.getElementById('THUG');
 img.onclick = function () {
+    var interval = setInterval(moveLeft, 100);
     img.style.marginLeft = '100px';
 }
