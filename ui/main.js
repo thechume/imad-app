39e22fd369716f7917e1cc8327345234a32d7375
thechume <36409967+thechume@users.noreply.github.com>
2018-02-18 23:02:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,5 +14,5 @@ function moveRight () {
 }
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
-    img.style.marginLeft = '100px';
+    //img.style.marginLeft = '100px';
 }
