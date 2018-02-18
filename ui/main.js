6f8769fc5680956d7f35a27abaead5ea78880c46
thechume <36409967+thechume@users.noreply.github.com>
2018-02-18 23:42:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,17 +8,19 @@ var interval;
 function moveRight () {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
+    if( marginLeft >= 200) {
     img.onclick = function () {
         interval = setInterval(moveLeft, 20);
-    }
+    }}
 }
 
 function moveLeft () {
     marginLeft = marginLeft - 1;
     img.style.marginLeft = marginLeft - 'px';
+    if( marginLeft <= 100) {
     img.onclick = function () {
         interval = setInterval(moveRight, 20);
-    }
+    }}
 }
 
 img.onclick = function () {
