c39687292212500d2c634824570d4686b71c9029
thechume <36409967+thechume@users.noreply.github.com>
2018-02-20 21:38:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names=[];
+app.get("/submit-name/", function(req,res) { //URL => /submit-name?name=somehting
+    //get the name from the request
+    var name = req.params.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -26,16 +37,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names=[];
-app.get("/submit-name/", function(req,res) { //URL => /submit-name?name=somehting
-    //get the name from the request
-    var name = req.params.name;
-    
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
 });
 
 
