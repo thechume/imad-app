fec9c986a54bb9c0e9464e384bfecfe0b379dfbd
thechume <36409967+thechume@users.noreply.github.com>
2018-02-20 21:27:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,24 @@ button.onclick = function() {
     request.send(null);
 };
 
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit.btn');
+submit.onclick = function () {
+    //make a req to server and send name
+    
+    //capture list of names and render as list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i<names.length ; i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
+
 /*var button = document.getElementById('counter');
  var counter = 0;
  button.onclick = function() {
