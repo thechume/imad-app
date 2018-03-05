30f837dc2b459d6987bcea766a64c96aa3405f06
thechume <36409967+thechume@users.noreply.github.com>
2018-03-05 20:41:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*var express = require('express');
+var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').pool;
@@ -97,7 +97,7 @@ var pool = new Pool(config);
 app.get('/test-db', function(req,res){
     //make a request\
     //return the resposnse with results
-   pool.query('SELECT*FROM Test', function(err,result){
+   pool.query('SELECT * FROM Test', function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }
@@ -127,7 +127,7 @@ app.get('/articles/:articleName', function (req, res) {
     //article == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-    pool.query("SELECT*FROM article WHERE tilte = $1 " [req.params.articleName], function(err,result){
+    pool.query("SELECT * FROM article WHERE tilte = '"+ req.params.articleName+ "'", function(err,result){
         if(err){
            res.status(500).send(err.toString());
        }
@@ -163,98 +163,9 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
-});*/
-
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var Pool = require('pg').Pool;
-var app = express();
-var config = {
-    user: 'ssum16cs',
-    database: 'ssum16cs',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
-app.use(morgan('combined'));
-
-var counter = 0;
-
-
-
-app.get('/counter', function(req,res) {
-    counter = counter + 1;
-    res.send(counter.toString());
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
-var names=[];
-app.get("/submit-name/", function(req,res) { //URL => /submit-name?name=somehting
-    //get the name from the request
-    var name = req.params.name;
-    
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
-});
-
-app.get('/articles/:articleName', function(req,res) {
-    
-    pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'", function(err,res) {
-        if(err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else
-        {
-            if(res.rows.length===0)
-                res.status(404).send('Article not found');
-            else
-             {   var articleData= result.rows[0]; 
-             res.send = createTemplate(articles[articleName]); }
-        }
-    });
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var pool= new Pool(config);
-app.get('/test-db',function(req,res){
-    //make a select request
-    //return a respone with the results
-    pool.query('SELECT * FROM TEST', function(err, result) {
-        if(err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else
-        {
-            res.send(JSON.stringify(result));
-        }
-    });
-});
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
 
 
 
