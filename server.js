b8b2556628146cca694ce643c23c5ff6a202df77
thechume <36409967+thechume@users.noreply.github.com>
2018-03-05 21:10:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/submit-name', function(req, res) {
     res.send(JSON.stringify(names));
 }); 
 
-app.get('/articles/:articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     //article == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
