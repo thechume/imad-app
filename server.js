a7651cb576e7165f56d36ece9cac6cb82a26ef87
thechume <36409967+thechume@users.noreply.github.com>
2018-03-05 19:49:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/submit-name/", function(req,res) { //URL => /submit-name?name=somehtin
 
 app.get('/articles/:articleName', function(req,res) {
     
-    pool.query("SELECT * FROM article WHERE title="+req.params.articleName, function(err,res) {
+    pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'", function(err,res) {
         if(err)
         {
             res.status(500).send(err.toString());
