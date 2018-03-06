b8f01e466e38bc7877ed67264cb72ff2ddba52f7
thechume <36409967+thechume@users.noreply.github.com>
2018-03-06 19:59:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/articles/:articleName', function (req, res) {
     //article == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-    pool.query("SELECT*FROM article WHERE tilte = $1 " [req.params.articleName], function(err,result){
+    pool.query("SELECT*FROM article WHERE tilte = " [req.params.articleName], function(err,result){
         if(err){
            res.status(500).send(err.toString());
        }
