deb01c8f1b53f0cbb71007a7396a99174ce7afd7
thechume <36409967+thechume@users.noreply.github.com>
2018-03-06 20:18:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,19 +131,19 @@ app.get('/articles/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     pool.query("SELECT*FROM article WHERE title = " [req.params.articleName], function(err,result){
         if(err){
-           res.status(500).send(err.toString());
+           result.status(500).send(err.toString());
        }
        else{
            if(result.rows.length === 0){
-            res.status(404).send('Article not found');
+            result.status(404).send('Article not found');
            }
            else{
                var articleData = result.rows[0];
-               res.send(createTemplate(articleData));
+               result.send(createTemplate(articleData));
            }
        }
     });
-    res.send(createTemplate(articleData));
+    result.send(createTemplate(articleData));
 });
  
 app.get('/ui/style.css', function (req, res) {
