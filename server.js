9f75a20691b01fa34345bce2b824c6bfbf37b596
thechume <36409967+thechume@users.noreply.github.com>
2018-03-06 21:06:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function createTemplate(data){
     <html>
         <head>
             <title>
-                ${data.title}
+                ${title}
             </title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link href="/ui/style.css" rel="stylesheet" />
