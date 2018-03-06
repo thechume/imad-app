8113cc09854e0e00935c67bb531138414128b7ad
thechume <36409967+thechume@users.noreply.github.com>
2018-03-06 20:40:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,6 +143,7 @@ app.get('/articles/:articleName', function (req, res) {
            }
        }
     });
+    var articleData = result.rows[0];
     res.send(createTemplate(articleData));
 });
  
